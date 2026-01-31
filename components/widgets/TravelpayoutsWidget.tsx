a21f8f73142e4335d01f3cdafe577b8846488a13
Joanna Taratuta <joannataratuta@gmail.com>
2026-01-31 20:23:32 +0100

    Fix: removed problematic comment in GYGWidget

--- components/widgets/TravelpayoutsWidget.tsx
+++ components/widgets/TravelpayoutsWidget.tsx
@@ -1,20 +1,15 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function GYGWidget({ destination }: { destination: string }) {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://widget.getyourguide.com/dist/pa.umd.production.min.js';
+    const script = document.createElement("script");
+    script.src =
+      "https://widget.getyourguide.com/dist/pa.umd.production.min.js";
     script.async = true;
     document.body.appendChild(script);
   }, []);
 
-  return (
-    <div 
-      data-gyg-widget="activities"
-      data-gyg-q={destination}
-      {/* Pozostałe atrybuty */}
-    />
-  );
+  return <div data-gyg-widget="activities" data-gyg-q={destination} />;
 }
